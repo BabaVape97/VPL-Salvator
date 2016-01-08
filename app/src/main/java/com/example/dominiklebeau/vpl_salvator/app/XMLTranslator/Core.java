61c00610f15f8cb6eebedfa653a6ce54a3244eae
BuildTools <unconfigured@null.spigotmc.org>
2016-01-08 20:06:28 +0100

    Template für zukünftige Entwicklung vom XML-Translator

---
+++ app/src/main/java/com/example/dominiklebeau/vpl_salvator/app/XMLTranslator/Core.java
@@ -0,0 +1,21 @@
+package com.example.dominiklebeau.vpl_salvator.app.XMLTranslator;
+
+/**
+ * Created by IPat on 08.01.2016.
+ */
+public class Core {
+
+    VplMaster vpl; //Der Vertretungsplan
+
+    // Hier kommt der klassische Translator hin - so wie du ihn davor hattest :D
+
+    /*
+
+    WICHTIG:
+    Der XML-Translator muss so aufgebaut sein, dass er den <printjob> tag komplett ignoriert, dasselbe
+    gilt NICHT für den <page> tag (denn es könnte mehrere seiten geben.)
+
+    TODO: *notiz an mich: schreibe anleitung zur nutzung von diesem ding*
+
+     */
+}
