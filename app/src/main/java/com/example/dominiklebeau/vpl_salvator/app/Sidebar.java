72ecde73d2f53d839aa7d075265ab6009af187be
Dominik le beau <dominik.thiel97@googlemail.com>
2016-01-09 12:43:15 +0100

    bling + ArrayAdapter+options+strings+Main
     +Sidebar"tabs"
     P.S. in activity_sidebar_drawer.xml gibt es ein Problem mit dem Menutag

--- app/src/main/java/com/example/dominiklebeau/vpl_salvator/app/Sidebar.java
+++ app/src/main/java/com/example/dominiklebeau/vpl_salvator/app/Sidebar.java
@@ -80,17 +80,17 @@ public class Sidebar extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_vertretungsplan) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_klausurplan) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_abgabetermine) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_feiertage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_ferien) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_dokumente) {
 
         }
 
